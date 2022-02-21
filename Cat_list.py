ad1e121b3f75b9f9df81aac4f35a1e6daa44d215
trubalexan <95087334+trubalexan@users.noreply.github.com>
2022-02-21 20:25:29 +0400

    Add files via upload

---
+++ Cat_list.py
@@ -0,0 +1,14 @@
+from class_Cat import Cat
+
+Cat1 = Cat('Baron', 'm', 2)
+
+Cat2 = Cat('Sam', 'm', 2)
+
+
+print("Cat1.name=", Cat1.name)
+print("Cat1.gender=", Cat1.gender)
+print("Cat1.age=", Cat1.getAge())
+print("---------------------------------------------------")
+print("Cat2.name=", Cat2.getName())
+print("Cat2.gender=", Cat2.getGender())
+print("Cat2.age=", Cat2.age)
